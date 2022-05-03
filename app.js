07df08e7b07070834d5f7704eb111d55bbb84783
riti044 <93737621+riti044@users.noreply.github.com>
2022-05-03 15:03:49 +0400

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var server  = require('http').createServer(app);
 var io      = require('socket.io')(server);
 var port    =  process.env.PORT||5000;
 
+
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  const { name = 'World' } = req.query;
+  res.send(`Hello ${name}!`);
+}
+
 //Add recent chats to messages array
 var messages = [];
 var prevChat = 10;
